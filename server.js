811de06a2afeb125689c96840d98492977f22de5
Austin <brittonjaustin@gmail.com>
2023-07-13 02:56:35 -0700

    updated file path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 5000;
 
 const _dirname = path.dirname("");
 
-const buildPath = path.join(_dirname, '../parker-app/parker-app/build');
+const buildPath = path.join(_dirname, '../parker-app/build');
 console.log('what is the path: ', buildPath)
 
 app.use(express.static(buildPath));
@@ -20,7 +20,7 @@ app.get('/projectname', (req, res) => {
 app.get('/*', (req, res) => {
 
 	res.sendFile(
-		path.join(__dirname, '../parker-app/parker-app/build/index.html'),
+		path.join(__dirname, '../parker-app/build/index.html'),
 		function(err){
 			if(err){
 				res.status(500).send(err);
