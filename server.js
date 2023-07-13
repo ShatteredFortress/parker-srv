91064cc8f8a19762d68515b8cf2c13bb2d896e49
Austin <brittonjaustin@gmail.com>
2023-07-13 01:44:45 -0700

    simple to return name of project and server react

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+
+const _dirname = path.dirname("");
+
+const buildPath = path.join(_dirname, '../parker-app/parker-app/build');
+console.log('what is the path: ', buildPath)
+
+app.use(express.static(buildPath));
+
+
+app.get('/projectname', (req, res) => {
+	res.send('PARKER')
+})
+
+app.get('/*', (req, res) => {
+
+	res.sendFile(
+		path.join(__dirname, '../parker-app/parker-app/build/index.html'),
+		function(err){
+			if(err){
+				res.status(500).send(err);
+			}
+		}
+	)
+});
+
+
+app.listen(port, () => {
+	  console.log(`Server is running on port ${port}`);
+});
+
+
